576fd7d38e9b1fe339cdb12acdeb935f26274704
Anvarbek <anvarbektoshmamatov888@gmail.com>
2024-03-11 15:38:39 +0500

    vedio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 
 import masjid from "../Imges/masjid.jpg";
+import MuftiyVedyo from "../Imges/MUFTIY.mp4"
 
 const Home = () => {
   return (
     <div className="home" >
-      <div className="container " >
+      <div className="container " id="homeCont">
         <img src={masjid} alt="" data-aos="fade-right"/>
         <div className="homeText" data-aos="fade-left">
           <p>Nima uchun biz bu saytni yaratdik?</p>
@@ -14,6 +15,14 @@ const Home = () => {
             internetdan izlaydigon narsalarini bita vebsaytda jamlash uchun bu
             katta ma`suliyatni o`z zimmamizga oldik.{" "}
           </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="vedios">
+          <p>Ўзбекистон мусулмонлари идораси раиси, муфтий Нуриддин Холиқназар ҳазратларининг Рамазон ойи табриклари</p>
+          <video  controls >
+          <source src={MuftiyVedyo} type="video/mp4"/>
+          </video>
         </div>
       </div>
     </div>
