175ce3da00f02039bf0700aca33e2b2878b8b59f
Anvarbek <anvarbektoshmamatov888@gmail.com>
2024-04-04 09:27:06 +0500

    youtube vedio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import masjid from "../Imges/masjid.jpg";
 import MuftiyVedyo from "../Imges/MUFTIY.mp4";
 import HomeAnime from "../components+/HomeAnimeText";
+import ReactPlayer from "react-player";
 
 const Home = () => {
   return (
     <div className="home">
-      <HomeAnime/>
+      <HomeAnime />
       <div className="container " id="homeCont">
         <img src={masjid} alt="" data-aos="fade-right" />
         <div className="homeText" data-aos="fade-left">
@@ -32,6 +33,24 @@ const Home = () => {
           <video controls>
             <source src={MuftiyVedyo} type="video/mp4" />
           </video>
+          <p>
+            shayx Muhammad Sodiq Muhammad Yusuf hazratlarining Ramazon haqida
+            vedyolari
+          </p>
+          <div className="youtub">
+            <ReactPlayer
+              url={"https://youtu.be/QldRFRWvBxs?si=WKVBbXSPxj5n_NvP"}
+              controls={true}
+              width='100%'
+              className="yotubevedyo"
+            />
+            <ReactPlayer
+              url={"https://youtu.be/EMfZ7o-kI78?si=-JZtBFq0iyINs8fy"}
+              controls={true}
+              width='100%'
+              className="yotubevedyo"
+            />
+          </div>
         </div>
       </div>
     </div>
